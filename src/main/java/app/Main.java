6276ac1a1989fdcc171cd9f00d3def4cf05446c4
Noureldin <noureeldinhossamm@gmail.com>
2025-04-22 16:22:55 +0200

    Initialize startup pages

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,13 +1,8 @@
 package App;
 
-import javax.swing.JFrame;
-import static logic.Config.*;
 public class Main {
 
     public static void main(String[] args) {
-        // SplashScreen sc = new SplashScreen();
 
-        // LandingPage page = new LandingPage();
-        // .....
     }
 }
