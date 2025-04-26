dbab14ca625ecd0e7959244ca29902663a447179
Noureldin <noureeldinhossamm@gmail.com>
2025-04-26 16:55:09 +0300

    Update component (Gradient Panel)

--- src/main/java/gui/components/GradientPanel.java
+++ src/main/java/gui/components/GradientPanel.java
@@ -1,16 +1,22 @@
 package gui.components;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.LinearGradientPaint;
 import java.awt.RenderingHints;
 import java.awt.geom.Point2D;
 import javax.swing.JPanel;
-import logic.Config;
+import static logic.Config.*;
 
 public class GradientPanel extends JPanel {
+
+    private boolean applyBorder;
+
+    public GradientPanel(boolean applyBorder) {
+        this.applyBorder = applyBorder;
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -25,20 +31,36 @@ public class GradientPanel extends JPanel {
         Point2D end = new Point2D.Float(0, height);
 
         float[] fractions = {0.0f, 0.5f, 1.0f};
-        Color firstColor = Config.LIGHT_GREEN;
+        Color firstColor = LIGHT_GREEN;
         Color secondColor = new Color(
-                (firstColor.getRed() + Config.DARK_GREEN.getRed()) / 2,
-                (firstColor.getGreen() + Config.DARK_GREEN.getGreen()) / 2,
-                (firstColor.getBlue() + Config.DARK_GREEN.getBlue()) / 2
+                (firstColor.getRed() + DARK_GREEN.getRed()) / 2,
+                (firstColor.getGreen() + DARK_GREEN.getGreen()) / 2,
+                (firstColor.getBlue() + DARK_GREEN.getBlue()) / 2
         );
 
-        Color[] colors = {firstColor, secondColor, Config.DARK_GREEN};
+        Color[] colors = {firstColor, secondColor, DARK_GREEN};
 
         LinearGradientPaint gradient = new LinearGradientPaint(start, end, fractions, colors);
         g2d.setPaint(gradient);
         g2d.fillRect(0, 0, width, height);
 
         g2d.dispose();
+        if (applyBorder) {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            int inset = 10;
+            g2.setColor(JET_BLACK);
+            g2.setStroke(new BasicStroke(3));
+            g2.drawRoundRect(
+                    inset,
+                    inset,
+                    getWidth() - 2 * inset,
+                    getHeight() - 2 * inset,
+                    0,
+                    0
+            );
+        }
     }
 
 }
