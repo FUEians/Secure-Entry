7371cf2cd1bc61b7bd072122c63f07361b083a5e
Noureldin <noureeldinhossamm@gmail.com>
2025-04-26 16:57:54 +0300

    Update the main method

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,27 @@
 package App;
 
+import gui.pages.*;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.Timer;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static LandingPage landingPage;
+    private static SplashScreen splashScreen;
 
+    public static void main(String[] args) {
+        splashScreen = new SplashScreen();
+        Action openLandingPageAction = new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                landingPage = new LandingPage();
+                splashScreen.dispose(); 
+            }
+        };
+        Timer timer = new Timer(3000, openLandingPageAction);
+        timer.setRepeats(false);
+        timer.start();
     }
 }
