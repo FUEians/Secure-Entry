8eacbd760780e135da9364b2286ccc52b6bc8210
noorr <noorr@Noor_Riyadh>
2025-05-08 19:36:34 +0300

    Password_Manager Class Added

---
+++ src/main/java/logic/PasswordManager.java
@@ -0,0 +1,83 @@
+/*
+PasswordManager (Logic):
+-Handles everything related to passwords 
+(encryption, decryption, storage, and operations on password data).
+-Manages a list of Category objects.
+-Handles saving/loading all password-related data 
+(accounts, categories) using JsonStorage (to a file like categories.json).
+-Uses EncryptionUtil for encrypting/decrypting passwords.
+ */
+package logic;
+
+import java.util.ArrayList;
+import java.util.List;
+import models.Category;
+import models.AccountEntry;
+import javax.crypto.SecretKey;
+import logic.EncryptionUtil;
+
+public class PasswordManager {
+
+    private List< Category> categories;
+
+    public PasswordManager() {
+        categories = JsonStorage.loadFromFile("Category", Category.class);
+        if (categories == null) {
+            categories = new ArrayList<>();
+        }
+    }
+
+    // Add a new Category
+    public void addCategory(Category category) {
+        categories.add(category);
+        saveCategories();
+
+    }
+
+    // Remove  Category
+    public void removeCategory(Category category) {
+        categories.remove(category);
+        saveCategories();
+
+    }
+
+    // Add an account To category
+    public void addAccountToCategory(String categoryName, AccountEntry account, SecretKey key)
+            throws Exception {
+
+        for (Category c : categories) {
+            if (c.getName().equals(categoryName)) {
+                String encrypted = EncryptionUtil.encrypt(account.getPassword(), key);
+                account.setPassword(encrypted);
+                c.addAccount(account);
+                saveCategories();
+                return;
+            }
+        }
+    }
+
+    public void removeAccountFromCategory(String categoryName, AccountEntry account) {
+        for (Category c : categories) {
+            if (c.getName().equals(categoryName)) {
+                c.removeAccount(account);
+                saveCategories();
+                return;
+            }
+        }
+    }
+
+    public String decryptPassword(String encryptedPassword, SecretKey key) throws Exception {
+        return EncryptionUtil.decrypt(encryptedPassword, key);
+    }
+
+    // Save all categories to file
+    private void saveCategories() {
+        JsonStorage.saveToFile("categories.json", categories);
+    }
+
+    // Get all categories
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+}
